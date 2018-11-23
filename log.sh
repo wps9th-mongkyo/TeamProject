16056461ad6159237154a059e29eed707436ffb3
JungMongKyo <dreamong91@gmail.com>
2018-11-24 01:50:26 +0900

    eb 연결(nginx, uwsgi, supervisor, postgresql, s3, EC2, docker, eb)

---
+++ log.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+LOG_ERROR='/srv/project/.log/error.log'
+LOG_INFO='/srv/project/.log/info.log'
+CMD='sudo docker exec $(sudo docker ps -q) tail -f'
+eb ssh --command "$CMD $LOG_ERROR $LOG_INFO"
+
+
+eb ssh --command
