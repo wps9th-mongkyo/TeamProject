8d79a15307ebae17ef8baefea7d522214edf0514
JungMongKyo <dreamong91@gmail.com>
2018-11-26 11:07:10 +0900

    "sprint1" start

--- app/members/models.py
+++ app/members/models.py
@@ -4,4 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     img_profile = models.ImageField(upload_to='user', blank=True)
-    nickname = models.CharField(max_length=20, unique=True, blank=True)
+    phone = models.PositiveIntegerField(max_length=13)
+    reviews = models.TextField(blank=True)
+
+
+class Restaurant:
+    pass
