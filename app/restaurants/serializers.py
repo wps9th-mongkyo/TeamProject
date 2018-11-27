694974cf415e27e5cd0b1f265687e306f4a14c74
Jeeho Park <devpark29@gmail.com>
2018-11-27 16:32:42 +0900

    Restaurant serializer, apis 작성

---
+++ app/restaurants/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+
+from .models import Restaurant
+
+
+class ResSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Restaurant
+        field = (
+            'name',
+            'address',
+            'address_detail',
+            'phone_num',
+            'food_type',
+            'price_level',
+            'parking',
+            'Business_hour',
+            'break_time',
+            'last_order',
+            'holiday',
+            'website',
+            'view_num',
+            'review_num',
+            'want_num',
+            'created_at',
+            'modified_at',
+            'gps_location',
+        )
