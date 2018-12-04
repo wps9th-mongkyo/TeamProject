f9f5bc4a3ea5f0c98bd33433c071bf9b49e81b1a
Jeeho Park <devpark29@gmail.com>
2018-12-04 15:27:42 +0900

    12.04 gitpush

--- app/restaurants/admin.py
+++ app/restaurants/admin.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 
-from .models import Restaurant, Menu, MenuImage
+from .models import Restaurant, MenuImage
 # Register your models here.
 
 
 admin.site.register(Restaurant)
-admin.site.register(Menu)
 admin.site.register(MenuImage)
 
