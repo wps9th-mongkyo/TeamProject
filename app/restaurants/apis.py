2436e84db65533a1de2d2970558526609b1f90e0
JungMongKyo <dreamong91@gmail.com>
2018-12-07 11:00:32 +0900

    RDS 수정

--- app/restaurants/apis.py
+++ app/restaurants/apis.py
@@ -12,7 +12,7 @@ class ResList(generics.ListCreateAPIView):
 
 
 class ResSetPagination(PageNumberPagination):
-    page_size = 5
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
