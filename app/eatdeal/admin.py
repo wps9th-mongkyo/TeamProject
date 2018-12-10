dc621601e00725a770cebc430cd2ec8b2c72bdf2
JungMongKyo <dreamong91@gmail.com>
2018-12-10 17:27:18 +0900

    Eatdeal admin수정

--- app/eatdeal/admin.py
+++ app/eatdeal/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 
 from .models import Eatdeal, EatdealImage
 
-admin.site.register(Eatdeal)
+
+class EatdealAdmin(admin.ModelAdmin):
+    list_display = ['id', 'restaurant', 'deal_name', 'created_at', 'modified_at',]
+    search_fields = ['restaurant', 'deal_name']
+    ordering = ['-id', 'restaurant', 'created_at']
+
+admin.site.register(Eatdeal, EatdealAdmin)
 admin.site.register(EatdealImage)
 
--- app/posts/admin.py
+++ app/posts/admin.py
@@ -5,9 +5,9 @@ from .models import Post, PostImage
 
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'author', 'restaurant', 'rate', 'created_at', 'modified_at',]
-    search_fields = ['author', 'restaurant',]
-    ordering = ['-id','rate', 'created_at',]
+    list_display = ['id', 'author', 'restaurant', 'rate', 'created_at', 'modified_at']
+    search_fields = ['author', 'restaurant']
+    ordering = ['-id', 'rate', 'created_at']
 
 
 admin.site.register(Post, PostAdmin)
