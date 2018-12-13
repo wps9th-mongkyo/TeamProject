8e1f10ffbb6db93c64f62942bc05e8cc52648d5b
JungMongKyo <dreamong91@gmail.com>
2018-12-13 17:07:00 +0900

    wannago admin, urls 추가

--- app/restaurants/admin.py
+++ app/restaurants/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Restaurant, MenuImage
+from .models import Restaurant, MenuImage, Wannago
+
 # Register your models here.
 
 
 admin.site.register(Restaurant)
 admin.site.register(MenuImage)
+admin.site.register(Wannago)
--- app/restaurants/urls.py
+++ app/restaurants/urls.py
@@ -7,6 +7,6 @@ app_name = 'restaurants'
 urlpatterns = [
     path('list/', apis.ResList.as_view()),
     path('list/<int:pk>/', apis.ResDetail.as_view()),
-    path('wannago/', apis.WannagoCreate.as_view()),
-    path('wannago/<int:pk>/', apis.WannagoDestroy.as_view()),
+    path('list/wannago/', apis.WannagoCreate.as_view()),
+    path('list/wannago/<int:pk>/', apis.WannagoDestroy.as_view()),
 ]
