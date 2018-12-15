5d72b0a7a7471bccb02eed80d69db031c3e5c4cb
JungMongKyo <dreamong91@gmail.com>
2018-12-16 00:37:23 +0900

    pipenv install djagno-filter

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -56,6 +56,10 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 5,
+
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ),
 }
 
 
