5632e9754773f0df6f714ebc342fa21040ef5e40
JungMongKyo <dreamong91@gmail.com>
2018-12-20 17:36:01 +0900

    pagination default 값 변경

--- app/config/settings/base.py
+++ app/config/settings/base.py
@@ -63,7 +63,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 5,
+    'PAGE_SIZE': 100,
 
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
